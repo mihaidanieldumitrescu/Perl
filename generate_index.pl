806f9652f770db9fea9a52f304d86dbed5d1e1a6
mihaidanieldumitrescu <dumitr.mihai@gmail.com>
2016-11-19 00:22:18 +0200

    Update generate_index.pl
    Script calls generate_split to generate html files.

--- generate_index.pl
+++ generate_index.pl
@@ -2,36 +2,38 @@ use strict;
 use warnings;
 
 use File::Copy;
-
+  
 my @filelist;
 
 sub main{
-open (OUT, ">", "index.html");
-print OUT "<html><body><table style=\"margin-left:200px\">";
-opendir (PIC, ".");
-
-	while (my $file = readdir(PIC)){
-	    #generate($file);
-		push @filelist, $file;
-	}
+	open (OUT, ">", "index.html");
+	print OUT "<html><body><table style=\"margin-left:200px\">";
+	opendir (PIC, ".");
 	
-closedir(PIC);
-
-foreach my $file (@filelist){
-	
-	if ($file =~ /500px/){
-		
-			#copy ("generate.pl", $file);
-		    #chdir (".\\".$file);
-			#print "Line: $file\n";
-			#system("cd");	
-			#system("perl -w generate.pl");	
-			print OUT "<tr><td><a href=\"$file\\output.html \">$file \(".countFiles("$file")."\)</a></td></tr>\n";
+		while (my $file = readdir(PIC)){
+			if (-d $file){
+				generate($file);
+				push @filelist, $file;
 			}
-	}
-
-print OUT "</table></body></html>";
-close (OUT);
+		}
+		
+	closedir(PIC);
+	
+	foreach my $file (@filelist){
+		
+		if ($file =~ /500px/){
+			
+				#copy ("generate.pl", $file);
+				#chdir (".\\".$file);
+				#print "Line: $file\n";
+				#system("cd");	
+				#system("perl -w generate.pl");	
+				print OUT "<tr><td><a href=\"$file\\output_index.html \">$file \(".countFiles("$file")."\)</a></td></tr>\n";
+				}
+		}
+	
+	print OUT "</table></body></html>";
+	close (OUT);
 }
 
 sub countFiles{
@@ -51,25 +53,29 @@ return $count;
 }
 
 sub generate{
+	my $arg = shift;
+	system ("perl -w generate_split.pl $arg");
+}
+
+sub generate_simple{
 	
 	my $indir = shift;
 	print "Entering function generate output.html\n\n";
 
-	opendir (PIC, ".\\$indir\\");
-
+		opendir (PIC, ".\\$indir\\");
 	
-	open (OUT, ">", ".\\$indir\\output.html");
-	print OUT "<html><body><table><tr>";
-	
-		while (my $file = readdir(PIC)){
-			if ($file =~ /\.jpg/)
-			{	
-				print OUT "<td><img src=\"$file\" height=\"600\"></img></td>";
+		open (OUT, ">", ".\\$indir\\output.html");
+		print OUT "<html><body><table><tr>";
+		
+			while (my $file = readdir(PIC)){
+				if ($file =~ /\.jpe?g/)
+				{	
+					print OUT "<td><img src=\"$file\" height=\"600\"></img></td>";
+				}
 			}
-		}
-	closedir(PIC);
-	print OUT "</tr></table></body></html>";
-	close (OUT);
+		closedir(PIC);
+		print OUT "</tr></table></body></html>";
+		close (OUT);
 
 }
 
