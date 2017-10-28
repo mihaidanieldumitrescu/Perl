aeac01f9148bb493ea468c531d8b88d1a9c3a747
Mihai-Daniel Dumitrescu <dumitr.mihai@gmail.com>
2017-10-28 13:32:16 +0300

    generated an additional 'random_gallery' page which will randomly show a set of 10 pictures

--- pictureGalleryHTML.pl
+++ pictureGalleryHTML.pl
@@ -9,16 +9,20 @@ my @filelist;
 my $heightSize = 650;
 my $files_per_segment=100;
 
-sub generatePageHeader{
+my $picture_ref = {	
+						pictures =>  []	
+				  };
+
+sub generatePageHeader {
 
 	#index html page
-	my $header="<html><body>\n<h3>pictureGalleryHTML.pl generated page</h3>";
-	$header.= "<p>Generated on ". strftime ("%Y.%m.%d",localtime). " </p>\n";
-	$header .= "<ul style=\"margin-left:100px\">\n";
+	my $header="<html><body>\n<h3>pictureGalleryHTML.pl generated page</h3>" .
+				"<p>Generated on ". strftime ("%Y.%m.%d",localtime). " </p>\n" .
+				"<ul style=\"margin-left:100px\">\n";
 	return $header;
 }
 
-sub generateNavigationBar{
+sub generateNavigationBar {
 	my $pictureCount = shift; #non jpeg files not included => could generate an additioanal non-existant page link
 	my $currentPage= shift;
 	my @pages;
@@ -26,8 +30,7 @@ sub generateNavigationBar{
  
 	my $index =100;
 	push @pages, "output_index.html";
-	while ($index <= $pictureCount ){
-	 
+	while ($index <= $pictureCount ) {
 		push @pages, "output_$index.html";
 		$index += 100;
 	}
@@ -38,39 +41,58 @@ sub generateNavigationBar{
 	my $naviBar= '
 	<div>
 		<ul>
-		    <li><a href="../../index.html">Home</a></li>';
+		    <li>
+				<a href="../../index.html">Home</a>
+			</li>';
 		foreach my $page (@pages) {
 			if ($page eq $currentPage){
 			    $page =~ s/index/1/; # index is page 1
 				$page =~ /(\d+)/;
 				if ($1 < 1000){
 					$naviBar .= '
-						<li><a class="active"> Page '.$1.'-'.($1+99).'</a></li>';
+						<li>
+							<a class="active"> Page '.$1.'-'.($1+99).'</a>
+						</li>';
 				} elsif ($1 == 1000) {
-						$naviBar .= '<li><a class="menu">...</a></li></ul></div>
-						<div class="hidden-menu"><ul>
-							<li><a class="active"> Page '.$1.'-'.($1+99).'</a></li>';
+						$naviBar .= '<li>
+										<a class="menu">...</a>
+									</li>
+								</ul>
+							</div>
+						<div class="hidden-menu">
+							<ul>
+								<li>
+									<a class="active"> Page '.$1.'-'.($1+99).'</a>
+								</li>';
 				} else {
 						$naviBar .= '
-						<li><a class="active"> Page '.$1.'-'.($1+99).'</a></li>';
+						<li>
+							<a class="active"> Page '.$1.'-'.($1+99).'</a>
+						</li>';
 				}
-			}
-			else {
+			} else {
 			    $page =~ s/index/1/; # index is page 1
 				$page =~ /(\d+)/;
 				if ($1 < 1000){
 					$naviBar .= '
-				<li><a href="'. (($1 eq "1") ? "output_index.html" : $page).'">Page '.$1.'-'.($1+99).'</a></li>';}
+				<li>
+					<a href="'. (($1 eq "1") ? "output_index.html" : $page).'"> Page '.$1.'-'.($1+99).' </a>
+				</li>';}
 				elsif ($1 == 1000) {
-					$naviBar .= '<li><a class="menu">...</a></li></li></ul></div>
+					$naviBar .= '<li>
+									<a class="menu">...</a>
+								</li>
+							</li>
+						</ul>
+							</div>
 							<div class="hidden-menu"><ul>
 										<li><a href="'. (($1 eq "1") ? "output_index.html" : $page).'">Page '.$1.'-'.($1+99).'</a></li>';
 				} else {
 					$naviBar .= '
-							<li><a href="'. (($1 eq "1") ? "output_index.html" : $page).'">Page '.$1.'-'.($1+99).'</a></li>';
+							<li>
+								<a href="'. (($1 eq "1") ? "output_index.html" : $page).'">Page '.$1.'-'.($1+99).'</a>
+							</li>';
 					}
-				
-				
 			}   
 		}
 		$naviBar.="
@@ -81,11 +103,14 @@ sub generateNavigationBar{
 	return $naviBar;
 }
 
-sub main{
+sub main {
+
+	#generating index file first, list of all picture directories
 	open (OUT, ">", "index.html");
 	print OUT generatePageHeader();
-	opendir (PIC, ".") or die "Cannot open dir\n$!\n\n";
 	
+	#counting for each dir number of files
+	opendir (PIC, ".") or die "Cannot open dir\n$!\n\n";
 		while (my $dirs = readdir(PIC)){
 			if (-d $dirs and countFiles($dirs) > 0 and not $dirs =~ /^(.|..)$/){
 					push @filelist, $dirs;
@@ -101,19 +126,27 @@ sub main{
 		die "No dirs detected!\n\n";
 	}
 	
+	#creating li element with links to every 'output_index.html' file
 	foreach my $dirs (@filelist){
- 
 		if ( $dirs =~ /.*/){
-	
-				print OUT "<li><a href=\"$dirs\\GalleryHTML\\output_index.html \">$dirs \(".countFiles("$dirs")."\)</a></li>\n";
-				}
+				my $numberOfFiles = countFiles("$dirs");
+				print OUT "<li>
+								<a href=\"$dirs\\GalleryHTML\\output_index.html \"> 
+										  $dirs \( ". $numberOfFiles ." \)</a>
+								<ol> <a href=\"$dirs\\GalleryHTML\\random_gallery.html \"> Random Gallery
+																		  </a>
+								</ol>
+							</li>\n";
 		}
+	}
 	
-	print OUT "</ul></body></html>";
+	print OUT "</ul>
+					</body>
+						</html>";
 	close (OUT);
 }
 
-sub countFiles{
+sub countFiles {
 
 	my $count=0;
 	my $dirname = shift;
@@ -129,23 +162,25 @@ sub countFiles{
 return $count;
 }
 
-sub generate{
+sub generate {
 	
-	#
+	# generate each gallery file 
 	my $dirname = shift;
-	my $file_index=1;
-	my $file_number=1;
+	my $file_index= 1;
+	my $file_number= 1;
 	my $regex_jpeg = "\.jpe?g\$";
 	
 	print "Generating html gallery for \"$dirname\"\n\n";
 	opendir (SUB, $dirname) or die "\n$! \n Cannot open dir\n";
-	my @pictures =readdir(SUB);
+	my @pictures = readdir(SUB);
 	my $currentPageName = "";
+	
+	#dirname/picture.jpeg
+	foreach my $file  (@pictures) {
 		
-	foreach my $file  (@pictures){
+			push @{ $picture_ref->{pictures} }, $file if $file =~ /\.jpe?g/i;
  
-			if ( $file_index < $file_number * $files_per_segment){
-			
+			if ( $file_index < $file_number * $files_per_segment) {
 				if ($file_index == 1 + (($file_number - 1) * $files_per_segment) and $file =~ /\.jpe?g/i){
 					print "Opening file output_$file_number.html at $file_index\n";
 					if ($file_number == 1){
@@ -162,7 +197,7 @@ sub generate{
 						print  GEN '<html><head><link rel="stylesheet" href="../../main.css" type="text/css"></head>'."\n<body>\n<table>\n<tr>\n";
 					} elsif ($file_number == 2 ) {
 						print  GEN '<html><head><link rel="stylesheet" href="../../main.css" type="text/css"></head>'."\n<body>\n<table>\n<tr>\n <td><div class=\"bookmarks\"> <a href=\"output_index.html#page_end\"> Prev </a></div></td>";
-					}else {
+					} else {
 						print  GEN '<html><head><link rel="stylesheet" href="../../main.css" type="text/css"></head>'."\n<body>\n<table>\n<tr>\n <td><div class=\"bookmarks\"> <a href=\"output_".(($file_number - 1) * $files_per_segment-100).".html#page_end\"> Prev </a></div></td>";
 					}
 					
@@ -187,8 +222,36 @@ sub generate{
 	
 					close(GEN);
 					$file_number++;
-			}		
-		}
+			}
+			
+			# generating html for random gallery
+			open (RAND, ">", ".\\$dirname\\GalleryHTML\\random_gallery.html");
+			# HEAD
+			my $buffer = '<html><head><link rel="stylesheet" href="../../main.css" type="text/css">';
+			
+			my $asJSarray =  Dumper $picture_ref->{pictures};
+			$asJSarray =~ s/.* = /var pictures = /;
+			$buffer .=	"<script> $asJSarray 
+
+							document.write ('<table><tr>');
+							for (var i =0; i < 10; i++){
+								imageName = pictures[Math.floor(Math.random()*pictures.length)];
+								document.write (\"<td><img src=\" + \"../Resized/\" + imageName + \" height=\'$heightSize\'></img></td>\");	
+							}
+							document.write ('</tr></table>');
+			  			</script>";
+			
+			# BODY
+			$buffer .= '</head>'."\n<body>";
+			for (@{ $picture_ref->{pictures}} )
+				{
+					#$buffer .= '<li> ' . $_ . ' </li>'. "\n";
+				}
+			$buffer .= "</body></html>";
+			print RAND $buffer;
+			close(RAND);
+
+	}
 		
 			print GEN "</tr></table></body></html>";
 			close (GEN);
@@ -196,7 +259,7 @@ sub generate{
 	return $file_index;
 }
 
-sub resizeImage{
+sub resizeImage {
 	my ($dirname, $file) = @_;
  
 	unless ( -e "$dirname\\Resized\\$file" ){
@@ -207,7 +270,7 @@ sub resizeImage{
 	return "<td><img src=\"../Resized/$file\" height=\"$heightSize\"></img></td>\n";
 }
 
-sub debug{
+sub debug {
  
 	opendir (SUB, '.') or die "\n$! \n Cannot open dir\n";
 	my @pictures =readdir(SUB);
